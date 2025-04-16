5f8312e467868b4f75f68478c80d32efdc1e0749
Aby <abyphilipaby1996@gmail.com>
2025-04-16 23:10:18 +0530

    updated the requirements and setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="youtube sentiment analysis",
+    version="0.0.0",
+    author="Aby Philip",
+    author_email="abyphilipaby1996@gmail.com",
+    description="A sentiment analysis project using YouTube comments",
+    long_description="A sentiment analysis project using YouTube comments",
+    packages=find_packages(),
+    install_requires= []
+)
